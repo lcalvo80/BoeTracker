6e378caf2efbc6f227a783800b6c871145db7041
luis_calvo <luiscalvo80@hotmail.com>
2025-09-03 12:12:40 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/ui/Home";
+import Home from "./pages/Home";
 import AboutUsPage from "./pages/AboutUsPage";
 import ContactPage from "./pages/ContactPage";
 import BOEPage from "./pages/BOEPage";
 import BOEDetailPage from "./pages/BOEDetailPage";
 import PricingPage from "./pages/PricingPage";
-import Navbar from "./components/Navbar"; // o "./components/layout/Navbar" según tu estructura
+import Navbar from "./components/layout/Navbar"; // o "./components/layout/Navbar" según tu estructura
 
 const App = () => {
   return (
