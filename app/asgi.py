e522334434bc02d411a24dfc6ced13151c2d179c
luis_calvo <luiscalvo80@hotmail.com>
2025-09-12 13:43:08 +0200

    a

--- app/asgi.py
+++ app/asgi.py
@@ -1,6 +1,5 @@
-# app/asgi.py
 from hypercorn.middleware import wsgi
 from app import create_app
 
 flask_app = create_app()
-app = wsgi.WSGIMiddleware(flask_app)  # ASGI app
+app = wsgi.WSGIMiddleware(flask_app)
